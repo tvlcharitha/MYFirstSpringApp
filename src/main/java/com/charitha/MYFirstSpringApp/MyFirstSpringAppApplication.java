986674760e79f1a4e8b4659e1ee8900b13dfc26a
tvlcharitha <tvlcharitha@gmail.com>
2018-02-25 18:13:28 +0000

    added

---
+++ src/main/java/com/charitha/MYFirstSpringApp/MyFirstSpringAppApplication.java
@@ -0,0 +1,12 @@
+package com.charitha.MYFirstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSpringAppApplication.class, args);
+	}
+}
---
+++ src/main/java/com/charitha/MYFirstSpringApp/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.charitha.MYFirstSpringApp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "hello";
+    }
+}
